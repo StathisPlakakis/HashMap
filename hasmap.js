cddcfa99f5a6c8c65babe5eac2b285b36c9a1cc9
StathisPlakakis <stathisplakorg1997@gmail.com>
2024-06-16 14:12:54 +0300

    Add hash func, generates codes 0-hashMap.length-1

--- hasmap.js
+++ hasmap.js
@@ -1,8 +1,22 @@
 #!/usr/bin/env node
 class HashMap {
   constructor () {
-    this.hashMap = Array(16).fill().map(() => []);
-  };
+    let size = 16;
+    this.hashMap = Array(size).fill().map(() => []);
+  }
+
+  hash (key) {
+
+    let hashCode = 0;
+        
+    const primeNumber = 31;
+    for (let i = 0; i < key.length; i++) {
+      hashCode = (primeNumber * hashCode + key.charCodeAt(i)) % 
+                  newHashMap.hashMap.length;
+    }
+
+    return hashCode;
+  }
 }
 const newHashMap = new HashMap();
-console.log(newHashMap.hashMap)
+console.log(newHashMap.hash(''))
